64fe5c9de3e817be1bf62582aafdd9356a392b42
chientel <nguyenchiengthuy02@gmail.com>
2026-05-19 21:35:17 +0700

    add to test branch

--- main.cpp
+++ main.cpp
@@ -10,5 +10,8 @@ int main()
     }
     int a = 5;
     cout << "a = " << a << endl;
+    int b = a +              10;
+                int c = a*b;
+    cout << "c = " << c << endl;    
     return 0;
 }
